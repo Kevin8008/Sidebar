2eafdab82c4b043456bfe7595e7ed0221bc75d3d
Kevinsmil <94193732+Kevin8008@users.noreply.github.com>
2022-03-21 10:41:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let sidebar = document.getElementById("sidebar")
+
+function abrir() {
+    if (sidebar.className == "aberto") {
+        sidebar.className = "fechado"
+    } else {
+        sidebar.className = "aberto"
+    }
+}
